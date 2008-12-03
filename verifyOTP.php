6dc200ad484d6aab73e77a639f074e9eea9c37de
Paul Chen <chenpaul@gmail.com>
2008-12-03 18:41:02 +0000

    anti otp phishing

--- verifyOTP.php
+++ verifyOTP.php
@@ -156,8 +156,9 @@ if ($scDiff == 0) { // Same use session, check time stamp diff
 		$elapsed = time() - $lastTime;
 		debug('Elapsed time from last validation: ' . $elapsed . ' secs');
 		$deviation = abs($elapsed - $tsDelta);
+		$percent = truncate($deviation/$elapsed, 8) . '%';
 		debug("Key time deviation vs. elapsed time=".$deviation.' secs ('.
-			($deviation/$elapsed).'%)');
+			$percent.')');
 		if ($deviation > TS_TOLERANCE * $elapsed) {
 			debug("Is the OTP generated from a real crypto key?");
 			sendResp(S_SECURITY_ERROR);
