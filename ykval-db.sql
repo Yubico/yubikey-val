3d9f50790393abfc54adaada86877fd62e3372fc
Simon Josefsson <simon@josefsson.org>
2009-12-15 21:22:48 +0000

    Simplify.

--- ykval-db.sql
+++ ykval-db.sql
@@ -1,7 +1,3 @@
--- DROP DATABASE ykval;
-CREATE DATABASE ykval;
-USE ykval;
-
 CREATE TABLE clients (
   id INT NOT NULL AUTO_INCREMENT,
   active BOOLEAN DEFAULT TRUE,
@@ -39,21 +35,3 @@ CREATE TABLE queue (
   info VARCHAR(256) NOT NULL,
   PRIMARY KEY (id)
 );
-
--- DROP USER 'ykval_verifier'@'localhost';
-CREATE USER 'ykval_verifier'@'localhost';
-GRANT SELECT,INSERT,UPDATE(accessed, counter, low, high, sessionUse, nonce)
-       ON ykval.yubikeys to 'ykval_verifier'@'localhost';
-GRANT SELECT(id, secret, active)
-       ON ykval.clients to 'ykval_verifier'@'localhost';
-GRANT SELECT,INSERT,UPDATE,DELETE 
-	ON ykval.queue to 'ykval_verifier'@'localhost';
-
--- DROP USER 'ykval_revoke'@'localhost';
-CREATE USER 'ykval_revoke'@'localhost';
-GRANT UPDATE(active)
-        ON ykval.yubikeys to 'ykval_revoke'@'localhost';
-GRANT SELECT(publicName)
-        ON ykval.yubikeys to 'ykval_revoke'@'localhost';
-
-FLUSH PRIVILEGES;
