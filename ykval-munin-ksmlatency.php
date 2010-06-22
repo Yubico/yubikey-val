98460785442f44fa39b6be34dfb2f43a7aba274a
Simon Josefsson <simon@josefsson.org>
2010-06-22 16:01:39 +0000

    Fix typo.

--- ykval-munin-ksmlatency.php
+++ ykval-munin-ksmlatency.php
@@ -19,12 +19,12 @@ function ksmurl2shortname ($ksmurl) {
 $ksms = otp2ksmurls ("ccccccccfnkjtvvijktfrvvginedlbvudjhjnggndtck", 16);
 $shortksms = array_map("ksmurl2shortname", $ksms);
 
-if ($argc==2 && strcmp($argv[1], "autoconf")==0) {
+if ($argc==2 && strcmp($argv[1], "autoconf") == 0) {
   print "yes\n";
-  exit 0;
+  exit (0);
 }
 
-if ($argc==2 && strcmp($argv[1], "config")==0) {
+if ($argc==2 && strcmp($argv[1], "config") == 0) {
 
   echo "multigraph yk_latency\n";
   echo "graph_title KSM latency\n";
@@ -40,7 +40,7 @@ if ($argc==2 && strcmp($argv[1], "config")==0) {
     echo "${shortksm}_avgwait.draw LINE1\n";
   }
 
-  exit(0);
+  exit (0);
 }
 
 echo "multigraph diskstats_latency\n";
