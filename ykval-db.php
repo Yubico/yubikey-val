dd9f567e22db50ebd94afd8d12f637484416a058
Klas Lindfors <klas@yubico.com>
2012-06-12 09:22:46 +0200

    start work on integrating oracle patches from Remi Mollon <Remi.Mollon@cern.ch>

--- ykval-db.php
+++ ykval-db.php
@@ -9,28 +9,6 @@ require_once('ykval-log.php');
 
 class Db
 {
-
-
-  /**
-   * Constructor
-   *
-   * @param string $host Database host
-   * @param string $user Database user
-   * @param string $pwd  Database password
-   * @param string $name Database table name
-   * @return void
-   *
-   */
-  public function __construct($db_dsn, $db_username, $db_password, $db_options, $name='ykval-db')
-  {
-    $this->db_dsn=$db_dsn;
-    $this->db_username=$db_username;
-    $this->db_password=$db_password;
-    $this->db_options=$db_options;
-
-    $this->myLog=new Log($name);
-  }
-
   function addField($name, $value)
   {
     $this->myLog->addField($name, $value);
@@ -84,45 +62,6 @@ class Db
   {
     if ($this->dbh!=NULL) return True;
     else return False;
-  }
-  /**
-   * function to connect to database defined in config.php
-   *
-   * @return boolean True on success, otherwise false.
-   *
-   */
-  public function connect(){
-
-    try {
-      $this->dbh = new PDO($this->db_dsn, $this->db_username, $this->db_password, $this->db_options);
-    } catch (PDOException $e) {
-      $this->myLog->log(LOG_CRIT, "Database connection error: " . $e->getMessage());
-      $this->dbh=Null;
-      return false;
-    }
-    return true;
-  }
-
-  private function query($query, $returnresult=false) {
-    if(!$this->isConnected()) {
-      $this->connect();
-    }
-    if($this->isConnected()) {
-      $this->myLog->log(LOG_DEBUG, 'DB query is: ' . $query);
-
-      try {
-	$this->result = $this->dbh->query($query);
-      } catch (PDOException $e) {
-	$this->myLog->log(LOG_INFO, 'Database query error: ' . preg_replace('/\n/',' ',print_r($this->dbh->errorInfo(), true)));
-	$this->dbh = Null;
-	return false;
-      }
-      if ($returnresult) return $this->result;
-      else return true;
-    } else {
-      $this->myLog->log(LOG_CRIT, 'No database connection');
-      return false;
-    }
   }
 
   public function truncateTable($name)
@@ -274,90 +213,6 @@ or false on failure.
   }
 
   /**
-   * main function used to get rows by multiple key=>value pairs from Db table.
-   *
-   * @param string $table Database table to update row in
-   * @param array $where Array with column=>values to select rows by
-   * @param int $nr Number of rows to collect. NULL=>inifinity. Default=NULL.
-   * @param int $rev rev=1 indicates order should be reversed. Default=NULL.
-   * @param string distinct Select rows with distinct columns, Default=NULL
-   * @return mixed Array with values from Db row or 2d-array with multiple rows
-   *
-   */
-  public function findByMultiple($table, $where, $nr=null, $rev=null, $distinct=null)
-  {
-    $value=""; /* quiet the PHP Notice */
-    $match=null; /* quiet the PHP Notice */
-    $query="SELECT";
-    if ($distinct!=null) {
-      $query.= " DISTINCT " . $distinct;
-    } else {
-      $query.= " *";
-    }
-    $query.= " FROM " . $table;
-    if ($where!=null){
-      foreach ($where as $key=>$value) {
-	if ($key!=null) {
-	  if ($value!=null) $match.= " ". $key . " = '" . $value . "' and";
-	  else $match.= " ". $key . " is NULL and";
-	}
-      }
-      if ($match!=null) $query .= " WHERE" . $match;
-      $query=rtrim($query, "and");
-      $query=rtrim($query);
-    }
-    if ($rev==1) $query.= " ORDER BY id DESC";
-    if ($nr!=null) $query.= " LIMIT " . $nr;
-
-    $result = $this->query($query, true);
-    if (!$result) return false;
-
-    if ($nr==1) {
-      $row = $result->fetch(PDO::FETCH_ASSOC);
-      $result->closeCursor();
-      return $row;
-    }
-    else {
-      $collection=array();
-      while($row = $result->fetch(PDO::FETCH_ASSOC)){
-	$collection[]=$row;
-      }
-      $result->closeCursor();
-      return $collection;
-    }
-
-  }
-
-  /**
-   * main function used to delete rows by multiple key=>value pairs from Db table.
-   *
-   * @param string $table Database table to delete row in
-   * @param array $where Array with column=>values to select rows by
-   * @param int $nr Number of rows to collect. NULL=>inifinity. Default=NULL.
-   * @param int $rev rev=1 indicates order should be reversed. Default=NULL.
-   * @param string distinct Select rows with distinct columns, Default=NULL
-   * @return boolean True on success, otherwise false.
-   *
-   */
-  public function deleteByMultiple($table, $where, $nr=null, $rev=null)
-  {
-    $query="DELETE";
-    $query.= " FROM " . $table;
-    if ($where!=null){
-      $query.= " WHERE";
-      foreach ($where as $key=>$value) {
-	$query.= " ". $key . " = '" . $value . "' and";
-      }
-      $query=rtrim($query, "and");
-      $query=rtrim($query);
-    }
-    if ($rev==1) $query.= " ORDER BY id DESC";
-    if ($nr!=null) $query.= " LIMIT " . $nr;
-    return $this->query($query, false);
-  }
-
-
-  /**
    * Function to do a custom query on database connection
    *
    * @param string $query Database query
@@ -367,23 +222,6 @@ or false on failure.
   public function customQuery($query)
   {
     return $this->query($query, true);
-  }
-
-  /**
-   * Function to do a custom query on database connection
-   *
-   * @return int number of rows affected by last statement or 0 if database connection is not functional.
-   *
-   */
-  public function rowCount()
-  {
-    if($this->result) {
-      $count=count($this->result->fetchAll());
-      $this->result->closeCursor();
-      return $count;
-    } else {
-      return 0;
-    }
   }
 
   /**
