e51efce07930349a18923c8c2628d6aa921790e1
Fredrik Thulin <fredrik@yubico.com>
2012-06-18 14:19:09 +0200

    init

---
+++ ykval-munin-yubikeystats.php
@@ -0,0 +1,82 @@
+#!/usr/bin/php
+<?php
+
+set_include_path(get_include_path() . PATH_SEPARATOR .
+		 "/etc/ykval:/usr/share/ykval");
+
+require_once 'ykval-config.php';
+require_once 'ykval-db.php';
+
+if ($argc==2 && strcmp($argv[1], "autoconf") == 0) {
+  print "yes\n";
+  exit (0);
+}
+
+if ($argc==2 && strcmp($argv[1], "config") == 0) {
+
+  echo "graph_title YK-VAL YubiKey stats\n";
+  echo "graph_vlabel Known YubiKeys\n";
+  echo "graph_category ykval\n";
+
+  echo "yubikeys_enabled.label Enabled YubiKeys\n";
+  echo "yubikeys_enabled.draw AREA\n";
+
+  echo "yubikeys_disabled.label Disabled YubiKeys\n";
+  echo "yubikeys_disabled.draw STACK\n";
+
+  echo "yubikeys_1month.label YubiKeys seen last month\n";
+  echo "yubikeys_1month.draw LINE2\n";
+
+  echo "clients_enabled.label Enabled validation clients\n";
+  echo "clients_enabled.draw LINE2\n";
+
+  echo "clients_disabled.label Disabled validation clients\n";
+  echo "clients_disabled.draw LINE2\n";
+
+  exit (0);
+}
+
+# Connect to db
+$db = new Db($baseParams['__YKVAL_DB_DSN__'],
+	     $baseParams['__YKVAL_DB_USER__'],
+	     $baseParams['__YKVAL_DB_PW__'],
+	     $baseParams['__YKVAL_DB_OPTIONS__'],
+	     'ykval-munin-yubikeystats:db');
+if (!$db->connect()) {
+  logdie($myLog, 'ERROR Database connect error (1)');
+}
+
+function get_count($db, $table, $conditions) {
+  $res = $db->customQuery('SELECT count(1) FROM ' . $table . ' WHERE ' . $conditions);
+  if ($res) {
+    $r = $res->fetch(PDO::FETCH_ASSOC);
+    return $r['count'];
+  }
+
+  return Null;
+}
+
+if ($count = get_count($db, 'yubikeys', 'active=true')) {
+  echo "yubikeys_enabled.value " . $count . "\n";
+}
+
+if ($count = get_count($db, 'yubikeys', 'active=false')) {
+  echo "yubikeys_disabled.value " . $count . "\n";
+}
+
+if ($count = get_count($db, 'yubikeys', 'modified >= ' . (time() - (31 * 86400)))) {
+  echo "yubikeys_1month.value " . $count . "\n";
+}
+
+if ($count = get_count($db, 'clients', 'active=true')) {
+  echo "clients_enabled.value " . $count . "\n";
+}
+
+if ($count = get_count($db, 'clients', 'active=false')) {
+  echo "clients_disabled.value " . $count . "\n";
+}
+
+
+#%# family=auto
+#%# capabilities=autoconf
+?>
