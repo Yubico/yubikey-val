a4a12d2f2d00831a4d0c48280c81c0be9f9214cb
Jean Paul Galea <jeanpaul@yubico.com>
2015-07-15 15:44:29 +0200

    Refactor.
    - remove wrapper which provides no help,
    	just makes it harder to follow the code.

--- ykval-synclib.php
+++ ykval-synclib.php
@@ -434,7 +434,7 @@ class SyncLib
     /*
      Send out requests
     */
-    $ans_arr=$this->retrieveURLasync_wrap($urls, $ans_req, $timeout);
+    $ans_arr=retrieveURLasync("YK-VAL sync", $urls, $this->myLog, $ans_req, $match="status=OK", $returl=True, $timeout, $this->curlopts);
 
     if (!is_array($ans_arr)) {
       $this->log(LOG_WARNING, 'No responses from validation server pool');
@@ -537,10 +537,4 @@ class SyncLib
     if (isset($this->answers)) return $this->answers;
     else return 0;
   }
-
-  function retrieveURLasync_wrap ($urls, $ans_req=1, $timeout=1.0)
-  {
-    return retrieveURLasync("YK-VAL sync", $urls, $this->myLog, $ans_req, $match="status=OK", $returl=True, $timeout, $this->curlopts);
-  }
-
 }
