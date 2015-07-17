Yubico/yubikey-val

commit a28ad6df698b4ef20b6698a1f993019ad25bef25

Refactor.
- enable strict comparison for sscanf result.

- use single quotes where possible.

- styling.

ykval-common.php
@@ -224,7 +224,7 @@ function KSMdecryptOTP($urls, $logger, $curlopts)
 		$urls = array($urls);
 	}
 
-	$response = retrieveURLasync("YK-KSM", $urls, $logger, $ans_req=1, $match="^OK", $returl=False, $timeout=10, $curlopts);
+	$response = retrieveURLasync('YK-KSM', $urls, $logger, $ans_req=1, $match='^OK', $returl=False, $timeout=10, $curlopts);
 
 	if (is_array($response))
 	{
@@ -233,10 +233,15 @@ function KSMdecryptOTP($urls, $logger, $curlopts)
 
 	if ($response)
 	{
-		$logger->log(LOG_DEBUG, log_format("YK-KSM response: ", $response));
+		$logger->log(LOG_DEBUG, log_format('YK-KSM response: ', $response));
 	}
 
-	if (sscanf($response, "OK counter=%04x low=%04x high=%02x use=%02x", $ret["session_counter"], $ret["low"], $ret["high"], $ret["session_use"]) != 4)
+	if (sscanf($response,
+		'OK counter=%04x low=%04x high=%02x use=%02x',
+		$ret['session_counter'],
+		$ret['low'],
+		$ret['high'],
+		$ret['session_use'] !== 4)
 	{
 		return false;
 	}

commit db0f55b8187b39ea1be38975c532a97d6d97bf3a

Fix.
- fix bug introduced in a28ad6df698b4ef20b6698a1f993019ad25bef25.

ykval-common.php
@@ -251,7 +251,7 @@ function KSMdecryptOTP($urls, $logger, $curlopts)
 		$ret['session_counter'],
 		$ret['low'],
 		$ret['high'],
-		$ret['session_use'] !== 4)
+		$ret['session_use']) !== 4)
 	{
 		return false;
 	}