Yubico/yubikey-val

commit 6181abee14aacca55e81f8d051b2ed0d2002eefa

Refactor.
- strftime -> date.

- strftime is same as date, but with locales.

- date format doesn't output names (January, March, etc.),
	so locale doesn't matter.

- also, would we really want locale in logs?

ykval-verify.php
@@ -450,7 +450,7 @@ if ($sessionCounter == $seenSessionCounter && $sessionUse > $seenSessionUse)
 		'delta' => $tsDiff,
 		'secs' => $tsDelta,
 		'accessed' => sprintf('%s (%s)', $lastTime, $ad['accessed']),
-		'now' => sprintf('%s (%s)'. $now, strftime('%Y-%m-%d %H:%M:%S', $now)),
+		'now' => sprintf('%s (%s)'. $now, date('%Y-%m-%d %H:%M:%S', $now)),
 		'elapsed' => $elapsed,
 		'deviation' => sprintf('%s secs or %s%%', $deviation, round(100 * $percent)),
 	));

commit b106789217836fc706716f4132af0cde56596fbd

Fix.
- fix bug introduced in 6181abee14aacca55e81f8d051b2ed0d2002eefa.

- essentially, strftime() uses different format than date()

ykval-verify.php
@@ -450,7 +450,7 @@ if ($sessionCounter == $seenSessionCounter && $sessionUse > $seenSessionUse)
 		'delta' => $tsDiff,
 		'secs' => $tsDelta,
 		'accessed' => sprintf('%s (%s)', $lastTime, $ad['accessed']),
-		'now' => sprintf('%s (%s)'. $now, date('%Y-%m-%d %H:%M:%S', $now)),
+		'now' => sprintf('%s (%s)'. $now, date('Y-m-d H:i:s', $now)),
 		'elapsed' => $elapsed,
 		'deviation' => sprintf('%s secs or %s%%', $deviation, round(100 * $percent)),
 	));